9d290ea682b62569b33d6a14c7ba64a51bc75ed8
marcos brendon de paula azevedo <marcos.brendon@aluno.ufms.br>
2019-10-15 16:01:39 -0300

    update

--- batera.js
+++ batera.js
@@ -1,4 +1,5 @@
 var x = null
+
 const keymap={
     'h':'audios/bumbo.wav',
     'c':'audios/caixa.wav',
@@ -7,8 +8,9 @@ const keymap={
     'b':'audios/hithat.wav',
     'd':'audios/tom1.wav',
     's':'audios/tom2.wav',
-    'a':'audios/tom3.wav'
+    'a':'audios/tom3.wav',
 }
+
 document.addEventListener("keypress",(event)=>{
     let novo=new Audio(keymap[event.key])
     novo.play()
